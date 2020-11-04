3bf745cffc52983b1ffe14af0014d52348b004d4
cf19rodrigolindo <71392213+cf19rodrigolindo@users.noreply.github.com>
2020-11-04 19:01:43 +0100

    Add files via upload

---
+++ M14UF1E01.py
@@ -0,0 +1,9 @@
+print("2") #Literal cadena
+print(2) #Literal sencer
+print(0x123) #Literal sencer hexadecimal
+print(0o123) #Literal sencer octal
+print ( "M'agrada \"Monty Python\"") #Usem tecla escapament per comilla doble
+print('Me gusta "Monty Python"') #Usem tecla escapament per comilla doble
+#Prova comentari
+"""comentari
+multilinia"""
