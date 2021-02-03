187dcba71bef47391cc75ba664472b86eed02090
cf19rodrigolindo <71392213+cf19rodrigolindo@users.noreply.github.com>
2021-02-03 18:39:23 +0100

    Add files via upload

---
+++ 2.1.4.9.py
@@ -0,0 +1,8 @@
+﻿kilometros = float(input("Escribe los kilómetros a convertir"))
+millas = float(input("Escribe las millas a convertir"))
+
+millas_a_kilometros = millas * 0.621371
+kilometros_a_millas = kilometros * 1.60934
+
+print(millas, " millas son ", round(millas_a_kilometros, 2), " kilómetros ")
+print(kilometros, " kilómetros son ", round(kilometros_a_millas, 2), " millas ")
